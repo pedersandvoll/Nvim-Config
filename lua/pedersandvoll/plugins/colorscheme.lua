73a722be6c4c28fe0f1d16aefe4882637e7fc034
Peder Sandvoll <97838211+pedersandvoll@users.noreply.github.com>
2024-06-07 14:58:42 +0200

    New configs

--- lua/pedersandvoll/plugins/colorscheme.lua
+++ lua/pedersandvoll/plugins/colorscheme.lua
@@ -1,22 +1,52 @@
 return {
+    -- {
+    --     "ellisonleao/gruvbox.nvim",
+    --     priority = 1000,
+    --     config = function()
+    --         vim.cmd.colorscheme("gruvbox")
+    --         vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    --         vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    --         vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
+    --         vim.api.nvim_set_hl(0, "GruvboxGreenSign", { bg = "none" })
+    --         vim.api.nvim_set_hl(0, "GruvboxOrangeSign", { bg = "none" })
+    --         vim.api.nvim_set_hl(0, "GruvboxPurpleSign", { bg = "none" })
+    --         vim.api.nvim_set_hl(0, "GruvboxYellowSign", { bg = "none" })
+    --         vim.api.nvim_set_hl(0, "GruvboxRedSign", { bg = "none" })
+    --         vim.api.nvim_set_hl(0, "GruvboxBlueSign", { bg = "none" })
+    --         vim.api.nvim_set_hl(0, "GruvboxAquaSign", { bg = "none" })
+    --     end,
+    -- },
     {
         "ellisonleao/gruvbox.nvim",
         priority = 1000,
         config = function()
+            -- Setup gruvbox with hard contrast and custom overrides
+            require("gruvbox").setup({
+                contrast = "hard",
+                overrides = {
+                    ["@punctuation.bracket"] = { fg = "#fe8019" },
+                    ["@punctuation.delimiter"] = { fg = "#fe8019" },
+                    ["@punctuation.special"] = { fg = "#fe8019" },
+                    ["Operator"] = { fg = "#fe8019" },
+                },
+            })
+
+            -- Apply the gruvbox colorscheme
             vim.cmd.colorscheme("gruvbox")
+
+            -- Manually set background colors to none for Normal, NormalFloat, SignColumn, and sign-related highlights
             vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
             vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+            vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
+            vim.api.nvim_set_hl(0, "GruvboxGreenSign", { bg = "none" })
+            vim.api.nvim_set_hl(0, "GruvboxOrangeSign", { bg = "none" })
+            vim.api.nvim_set_hl(0, "GruvboxPurpleSign", { bg = "none" })
+            vim.api.nvim_set_hl(0, "GruvboxYellowSign", { bg = "none" })
+            vim.api.nvim_set_hl(0, "GruvboxRedSign", { bg = "none" })
+            vim.api.nvim_set_hl(0, "GruvboxBlueSign", { bg = "none" })
+            vim.api.nvim_set_hl(0, "GruvboxAquaSign", { bg = "none" })
         end,
-    },
-    -- {
-    --     "ribru17/bamboo.nvim",
-    --     priority = 1000,
-    --     config = function()
-    --         vim.cmd.colorscheme("bamboo")
-    --         vim.api.nvim_set_hl(0, "Normal", { bg = "#282828" })
-    --         vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#282828" })
-    --     end,
-    -- },
+    }
     -- {
     --     "sainnhe/everforest",
     --     priority = 1000,
@@ -24,6 +54,14 @@ return {
     --         vim.cmd.colorscheme("everforest")
     --         vim.api.nvim_set_hl(0, "Normal", { bg = "#282828" })
     --         vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#282828" })
+    --         vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
+    --         vim.api.nvim_set_hl(0, "GruvboxGreenSign", { bg = "none" })
+    --         vim.api.nvim_set_hl(0, "GruvboxOrangeSign", { bg = "none" })
+    --         vim.api.nvim_set_hl(0, "GruvboxPurpleSign", { bg = "none" })
+    --         vim.api.nvim_set_hl(0, "GruvboxYellowSign", { bg = "none" })
+    --         vim.api.nvim_set_hl(0, "GruvboxRedSign", { bg = "none" })
+    --         vim.api.nvim_set_hl(0, "GruvboxBlueSign", { bg = "none" })
+    --         vim.api.nvim_set_hl(0, "GruvboxAquaSign", { bg = "none" })
     --     end,
     -- },
 }
--- lua/pedersandvoll/plugins/mini.lua
+++ lua/pedersandvoll/plugins/mini.lua
@@ -2,7 +2,7 @@ return {
     {
         "echasnovski/mini.nvim",
         config = function()
-            require("mini.indentscope").setup()
+            -- require("mini.indentscope").setup()
             require("mini.completion").setup()
             require("mini.cursorword").setup()
             require("mini.files").setup()
